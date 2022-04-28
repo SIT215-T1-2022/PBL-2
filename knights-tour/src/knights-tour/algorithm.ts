9cd90c73ff58226f73a105d95e4af282ca9621be
Hugo <hugo.luccarini@gmail.com>
2022-04-28 22:14:49 +0200

    Add comments

--- knights-tour/src/knights-tour/algorithm.ts
+++ knights-tour/src/knights-tour/algorithm.ts
@@ -1,3 +1,4 @@
 export default abstract class KnightsTourAlgorithm {
+  // provides a virtual / abstract function for our class implementations to override  
   abstract getPath(n:number, startingX:number, startingY:number):Array<[number, number]>;
 }
