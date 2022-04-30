49d9c32e3fd09ab0408f7ac069d16009912bb8c9
Mark Blashki <markblashki1@gmail.com>
2022-04-30 11:13:30 +1000

    add: timing

--- knights-tour/src/App.tsx
+++ knights-tour/src/App.tsx
@@ -17,98 +17,103 @@ function App() {
   
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          // Ensure algorithm is not null
-          if(!algorithm){
-            setStatus("Algorithm not initialised!");
-            return;
-          }
-          const N:number = Number((document.getElementById("N") as HTMLInputElement).value);
-          const X:number = Number((document.getElementById("X") as HTMLInputElement).value);
-          const Y:number = Number((document.getElementById("Y") as HTMLInputElement).value);
-          
-          // Run the algorithm
-          setStatus("Running...");
-          let sol;
-          setTimeout(() => {
-            sol = algorithm.run(N ?? 8, X ?? 0, Y ?? 0);
+      <div style={{margin: "auto", width: "300px", marginTop: "1em"}}>
+        <button
+          onClick={() => {
+            // Ensure algorithm is not null
+            if(!algorithm){
+              setStatus("Algorithm not initialised!");
+              return;
+            }
+            const N:number = Number((document.getElementById("N") as HTMLInputElement).value);
+            const X:number = Number((document.getElementById("X") as HTMLInputElement).value);
+            const Y:number = Number((document.getElementById("Y") as HTMLInputElement).value);
             
-            // Print the output
-            setOutput(algorithm.printSolution(sol));
+            // Run the algorithm
+            setStatus("Running...");
+            let sol;
+            setTimeout(() => {
+              var start = new Date().getTime();
+              sol = algorithm.run(N ?? 8, X ?? 0, Y ?? 0);
+              var end = new Date().getTime();
+              // Print the output
+              setOutput(algorithm.printSolution(sol));
+              
+              setStatus("Done. Algororithm finished in " + (end - start) + "ms");
+            }, 100)
             
-            setStatus("Done.");
-          }, 100)
-          
-        }}
-      >
-        START
-      </button>
-      &nbsp;
-      <button
-        onClick={() => setAlgorithm(new Warnsdorff())}
-        disabled={algorithm && algorithm.name === "Warnsdorff's"}
-      >
-        Warnsdorff
-      </button>
-      &nbsp;
-      <button
-        onClick={() => setAlgorithm(new Backtrack())}
-        disabled={algorithm && algorithm.name === "Backtracking"}
-      >
-        Backtracking
-      </button>
-      <br /><br />
-      Board Size: <input
-        type="number"
-        id="N"
-        min="0"
-        max="500"
-      />
-      <br />
-      Start X: <input
-        type="number"
-        min="0"
-        id="X"
-        max={algorithm ? algorithm.N-1 : 0}
-      />
-      <br />
-      Start Y: <input
-        type="number"
-        min="0"
-        id="Y"
-        max={algorithm ? algorithm.N-1 : 0}
-      />
-      <pre>
-        Status: {status}
-      <pre>
-      </pre>
-        Algorithm: {algorithm ? algorithm.name : "uninitialised"}
-      </pre>
-      {/* Output */}
-      {output && output.split("\n").map((row, rowKey) => 
-        <div
-          style={{
           }}
-          key={rowKey}
         >
-          {row.split('|').map((value, valueKey) => 
-            value && <div
-              style={{
-                width: "3rem",
-                lineHeight: "3rem",
-                textAlign: "center",
-                display: "inline-block",
-                border: "1px solid black",
-                backgroundColor: (rowKey + valueKey) % 2 === 1 ? "#b37964" : "#edc5b7"
-              }}
-              key={valueKey}
-            >
-              {value}
-            </div>
-          )}
-        </div>
-      )}
+          START
+        </button>
+        &nbsp;
+        <button
+          onClick={() => setAlgorithm(new Warnsdorff())}
+          disabled={algorithm && algorithm.name === "Warnsdorff's"}
+        >
+          Warnsdorff
+        </button>
+        &nbsp;
+        <button
+          onClick={() => setAlgorithm(new Backtrack())}
+          disabled={algorithm && algorithm.name === "Backtracking"}
+        >
+          Backtracking
+        </button>
+        <br /><br />
+        Board Size: <input
+          type="number"
+          id="N"
+          min="0"
+          max="500"
+        />
+        <br />
+        Start X: <input
+          type="number"
+          min="0"
+          id="X"
+          max={algorithm ? algorithm.N-1 : 0}
+        />
+        <br />
+        Start Y: <input
+          type="number"
+          min="0"
+          id="Y"
+          max={algorithm ? algorithm.N-1 : 0}
+        />
+        <pre>
+          Status: {status}
+        <pre>
+        </pre>
+          Algorithm: {algorithm ? algorithm.name : "uninitialised"}
+        </pre>
+      </div>
+      <div style={{textAlign: "center"}}>
+        {/* Output */}
+        {output && output.split("\n").map((row, rowKey) => 
+          <div
+            style={{
+            }}
+            key={rowKey}
+          >
+            {row.split('|').map((value, valueKey) => 
+              value && <div
+                style={{
+                  width: "3rem",
+                  lineHeight: "3rem",
+                  textAlign: "center",
+                  display: "inline-block",
+                  border: "1px solid black",
+                  backgroundColor: (rowKey + valueKey) % 2 === 1 ? "#b37964" : "#edc5b7"
+                }}
+                key={valueKey}
+              >
+                {value}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
